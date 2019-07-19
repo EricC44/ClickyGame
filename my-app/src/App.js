04e240c1859ce88d94dc003faa2c63db3b88eae0
ECarlson <ericcarlson4@outlook.com>
2019-07-18 18:01:28 -0700

    working on rendering the push

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import pictures from './pictures.json';
-import Card from './Components/Card/index' 
+import Card from './Components/Card';
+import Header from './Components/Header';
+import Wrapper from './Components/Wrapper'; 
 
-function App() {
-  return (
-    <Wrapper>
-      <Navbar>
+class App extends Component {
+  //This is our initial state of the pictures
+  state = {
+    pics,
+    score: 0,
+    high: 0
+  }
 
-      </Navbar>
-    </Wrapper>
-  );
+ render() {
+   return (
+     <Wrapper>
+       <Header score={this.state.score} high={this.state.high}>Mario and Friends Clicky Game</Header>
+       {this.state.cards.map(card => (
+         <Card
+          onclick={this.onclick}
+          id={this.id}
+          photo={this.photo}
+          />
+       ))}
+     </Wrapper>
+   )
+ }
 }
 
 export default App;

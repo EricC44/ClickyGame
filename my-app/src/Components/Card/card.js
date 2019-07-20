653f4ff9d5bab7b31c194e3e50b13be8af60020a
ECarlson <ericcarlson4@outlook.com>
2019-07-19 17:10:47 -0700

    added some important references to the .json file

--- my-app/src/Components/Card/card.js
+++ my-app/src/Components/Card/card.js
@@ -3,7 +3,7 @@ import './style.css'
 // This function is to call the card to be able to be clicked on
 const Card = props => {
   // This is where the props get clicked on it will do something
-  <div className='card' onClick={() => props.clickTime(props.id)}>
+  <div className='card' onClick={() => props.clicky(props.id)}>
     <div className='img-container'>
       <img src={props.photo} alt={props.name} />
     </div>

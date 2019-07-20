448d46ad4f6402014258ab3fbbc613b3d4fde0aa
ECarlson <ericcarlson4@outlook.com>
2019-07-19 17:10:12 -0700

    added a sorting function for the pictures

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,21 +16,29 @@ class App extends Component {
     // This is to find the card that is being clicked
     this.state.cards.find((j, i) => {
       if (j.id === id) {
-        if (cards[i].beenClicked === false) {
-          cards[i].beenClicked = true
+        //If the card clicked is set to false, set it to true and add a point to the score
+        if (pictures[i].beenClicked === false) {
+          pictures[i].beenClicked = true
           this.setState({score : this.state.score + 1}, function(){
             console.log(this.state.score)
           })
+          // This function sorts out the pictures position randomly
+          this.state.pictures.sort(() => Math.random() - 0.5)
+          return true
+        } else {
+          this.endGame()
         }
       }
     })
   }
+  // This is our game over function
+    
 
  render() {
    return (
      <Wrapper>
        <Header score={this.state.score} high={this.state.high}>Mario and Friends Clicky Game</Header>
-       {this.state.cards.map(card => (
+       {this.state.pictures.map(card => (
          <Card
           clicky={this.onclick}
           id={this.id}

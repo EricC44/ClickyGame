85ecf20be93636bd20ba43dadbdcf33c3cb11e44
ECarlson <ericcarlson4@outlook.com>
2019-07-20 22:58:32 -0700

    added className to images

--- my-app/src/Components/Card/card.js
+++ my-app/src/Components/Card/card.js
@@ -5,7 +5,7 @@ function Cards (props) {
   return (
   // This is where the props get clicked on it will do something
     <div className='card-img' onClick={() => props.clicky(props.id)}>
-      <img src={props.photo} alt={props.name} />
+      <img src={props.photo} alt={props.name} className='images' />
     </div>
   )
 }

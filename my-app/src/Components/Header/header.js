02024a37a062ca413b3573e41ad9dcf56d341d93
ECarlson <ericcarlson4@outlook.com>
2019-07-20 22:59:15 -0700

    added JSX spacing between score and highscore

--- my-app/src/Components/Header/header.js
+++ my-app/src/Components/Header/header.js
@@ -1,13 +1,18 @@
-import React from "react";
-import "./header.css";
+import React from 'react'
+import './header.css'
 
 const Header = props => (
   <div className='header'>
     <div className='nameGame'>{props.children}</div>
     <div className='score'>
-            Score: {props.score}
-            Highscore: {props.high}
+      Score: {props.score}
+      &nbsp;
+      Highscore: {props.high}
     </div>
+    <br />
+    <div className='howTo'>Hello, welcome to the Mario Clicky Game! The whole objective of the game is to click on different images every time.
+    There is one catch however, every time you click an image it shuffles the images around.  If you click on an image that has already been
+    picked, your game is over.  Try to get a highscore, and LET'S-A-GO!!!</div>
   </div>
 )
 

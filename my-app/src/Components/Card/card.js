3502fd03e25598c2f46ccc052403697ca00ee252
ECarlson <ericcarlson4@outlook.com>
2019-07-20 18:07:48 -0700

    updated typos in page

--- my-app/src/Components/Card/card.js
+++ my-app/src/Components/Card/card.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import './style.css'
 // This function is to call the card to be able to be clicked on
-function Card (props) {
+function Cards (props) {
   return (
   // This is where the props get clicked on it will do something
-    <div className='card' onClick={() => props.clicky(props.id)}>
-      <div className='img-container'>
-        <img src={props.photo} alt={props.name} />
-      </div>
+    <div className='card-img' onClick={() => props.clicky(props.id)}>
+      <img src={props.photo} alt={props.name} />
     </div>
   )
 }
 
-export default Card
+export default Cards

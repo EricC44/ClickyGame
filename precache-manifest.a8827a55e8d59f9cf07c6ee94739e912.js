021ccb395720eefac6dfe0e7c96a4c3bdf5fbb68
ECarlson <ericcarlson4@outlook.com>
2019-08-06 00:09:51 -0700

    Updates

---
+++ precache-manifest.a8827a55e8d59f9cf07c6ee94739e912.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c00fea35b16e57d10fc00308bcfa28b5",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "5fe4f419e3e148ec6968",
+    "url": "/ClickyGame/static/css/main.6b877c1b.chunk.css"
+  },
+  {
+    "revision": "4e7117101b1c2b4992a1",
+    "url": "/ClickyGame/static/js/2.cf37b490.chunk.js"
+  },
+  {
+    "revision": "5fe4f419e3e148ec6968",
+    "url": "/ClickyGame/static/js/main.6d9c1e52.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
